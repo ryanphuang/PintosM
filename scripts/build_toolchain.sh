56d8a0edf36e8aa77697b585dce16f3007083d55
Ryan Huang <ryanhuang@cs.ucsd.edu>
2017-06-15 09:12:13 +0000

    Improve toolchain build script: - avoid repeated source downloading and unarchiving - support single target building - error handling

--- scripts/build_toolchain.sh
+++ scripts/build_toolchain.sh
@@ -11,82 +11,133 @@
 
 perror()
 {
-  if [ $? -ne 0 ]; then
-    echo $1
-    exit 1
-  fi
+  >&2 echo $1
+  exit 1
 }
 
 download_and_check()
 {
   local fname=$(basename $1)
+  local fdirname="${fname%.tar.*}"
   cd $CWD/src
-  # wget $1 
   if [ ! -f $fname ]; then
-    echo "Failed to download $1"
-    exit 1
+    wget $1 
+    if [ ! -f $fname ]; then
+      perror "Failed to download $1"
+    fi
   fi
   local checksum=$(shasum -a 256 $fname | cut -d' ' -f 1)
   if [ "$checksum" != "$2" ]; then
-    echo "Corrupted or missing source $1: expecting $2 got $checksum"
-    exit 1
+    perror "Corrupted or missing source $1: expecting $2 got $checksum"
   fi
   echo "Downloaded and verified $fname from $1"
-  if [ $fname == *.tar.gz ]; then
-    tar xzvf $fname
-  elif [ $fname == *.tar.bz2 ]; then
-    tar xjvf $fname
-  elif [ $fname == *.tar.xz ]; then
-    tar xJvf $fname
+  if [ ! -d $fdirname ]; then
+    echo "Extracting $fname to $fdirname..."
+    if [ $fname == *.tar.gz ]; then
+      tar xzf $fname 
+    elif [ $fname == *.tar.bz2 ]; then
+      tar xjf $fname
+    elif [ $fname == *.tar.xz ]; then
+      tar xJf $fname
+    else
+      perror "Unrecognized archive extension $fname"
+    fi
   else
-    echo "Unrecognized archive extension $fname"
+    echo "$fname is already extracted"
   fi
 }
 
+usage()
+{
+  cat <<EOF
+
+  Usage: $0 [options] [DEST_DIR] [TOOL]
+
+    -h: display this message
+
+    DEST_DIR: Base directory to store the downloeaded source code, build and 
+              distribute the compiled toolchain.
+    TOOL:     By default, this script build binutils, GCC, and GDB. 
+              Specify a single tool to download and build.
+
+EOF
+}
+
 if [ $# -eq 0 ]; then
-  CWD=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
-else
-  if [ ! -d $1 ]; then
-    echo "Usage: $0 [DEST_DIR]"
-    exit 1
-  fi
-  CWD=$(cd $1 && pwd)
+  >&2 usage
+  exit 1
 fi
 
+if [ "$1" == "-h" ]; then
+  usage
+  exit 0
+fi
+
+tool=all
+if [ $# -eq 2 ]; then
+  tool=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+  if [ $tool != "binutils" -a $tool != "gcc" -a $tool != "gdb" ]; then
+    perror "TOOL must be one of {binutils,gcc,gdb}"
+  fi
+fi
+
+os="`uname`"
+
+mkdir -p $1/{src,dist,build} || perror "Failed to create toolchain source and build directories"
+
+CWD=$(cd $1 && pwd)
 PREFIX=$CWD/dist
 export PATH=$PREFIX/bin:$PATH
-export DYLD_LIBRARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
+if [ $os == "Linux" ]; then
+  export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+elif [ $of == "Darwin" ]; then
+  export DYLD_LIBRARY_PATH=$PREFIX/lib:$DYLD_LIBRARY_PATH
+else
+  perror "Unsupported OS: $os"
+fi
+
 mkdir -p src build/{binutils,gcc,gdb} dist
 TARGET=i386-elf
 
 # Download sources
-download_and_check https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz 26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f
-download_and_check https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2 9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5
-download_and_check https://ftp.gnu.org/gnu/gdb/gdb-7.9.1.tar.xz cd9c543a411a05b2b647dd38936034b68c2b5d6f10e0d51dc168c166c973ba40
+if [ $tool == "all" -o $tool == "binutils" ]; then
+  download_and_check https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz 26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f
+fi
+if [ $tool == "all" -o $tool == "gcc" ]; then
+  download_and_check https://ftp.gnu.org/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2 9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5
+  if [ ! -d $CWD/src/gcc-6.2.0/mpfr ]; then
+    cd $CWD/src/gcc-6.2.0 && contrib/download_prerequisites || perror "Failed to download pre-requisite for GCC"
+  fi
+fi
+if [ $tool == "all" -o $tool == "gdb" ]; then
+  download_and_check https://ftp.gnu.org/gnu/gdb/gdb-7.9.1.tar.xz cd9c543a411a05b2b647dd38936034b68c2b5d6f10e0d51dc168c166c973ba40
+fi
 
-cd $CWD/build/binutils
-../../src/binutils-2.27/configure --prefix=$PREFIX --target=$TARGET \
-  --disable-multilib --disable-nls --disable-werror
-perror "Failed to configure binutils"
-make -j8
-perror "Failed to make binutils"
-make install
+if [ $tool == "all" -o $tool == "binutils" ]; then
+  echo "Building binutils..."
+  mkdir -p $CWD/build/binutils && cd $CWD/build/binutils
+  ../../src/binutils-2.27/configure --prefix=$PREFIX --target=$TARGET \
+    --disable-multilib --disable-nls --disable-werror || perror "Failed to configure binutils"
+  make -j8 || perror "Failed to make binutils"
+  make install
+fi
 
-cd $CWD/build/gcc
-../../src/gcc-6.2.0/configure --prefix=$PREFIX --target=$TARGET \
-  --disable-multilib --disable-nls --disable-werror --disable-libssp \
-  --disable-libmudflap --with-newlib --without-headers --enable-languages=c,c++
-perror "Failed to configure gcc"
-make -j8 all-gcc 
-perror "Failed to make gcc"
-make install-gcc
-make all-target-libgcc
-perror "Failed to libgcc"
-make install-target-libgcc
+if [ $tool == "all" -o $tool == "gcc" ]; then
+  echo "Building GCC..."
+  mkdir -p $CWD/build/gcc && cd $CWD/build/gcc 
+  ../../src/gcc-6.2.0/configure --prefix=$PREFIX --target=$TARGET \
+    --disable-multilib --disable-nls --disable-werror --disable-libssp \
+    --disable-libmudflap --with-newlib --without-headers --enable-languages=c,c++ || perror "Failed to configure gcc"
+  make -j8 all-gcc  || perror "Failed to make gcc"
+  make install-gcc
+  make all-target-libgcc || perror "Failed to libgcc"
+  make install-target-libgcc
+fi
 
-cd $CWD/build/gdb
-../../src/gdb-7.9.1/configure --prefix=$PREFIX --target=$TARGET --disable-werror
-perror "Failed to configure gdb"
-make -j8
-perror "Failed to make gdb"
-make install
+if [ $tool == "all" -o $tool == "gdb" ]; then
+  echo "Building gdb..."
+  mkdir -p $CWD/build/gdb && cd $CWD/build/gdb 
+  ../../src/gdb-7.9.1/configure --prefix=$PREFIX --target=$TARGET --disable-werror || perror "Failed to configure gdb"
+  make -j8 || perror "Failed to make gdb"
+  make install
+fi
